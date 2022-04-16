2ff6f15e3b1bd799ba6fce3d3e46f41edd9733f4
TC <dg910719@gmail.com>
2022-04-17 02:14:48 +0800

    a lot of things

---
+++ static/js/home.js
@@ -0,0 +1,11 @@
+// document.write("hi")
+function login(){
+    alert("hi");
+}
+
+function hello() {
+    alert("hello!");
+}
+
+var grid = document.getElementById("grid");
+grid.src = "./login.html"
---
+++ static/js/login.js
@@ -0,0 +1,13 @@
+var password = 123456;
+var input;
+
+// while(password != input) {
+//     input = prompt("請輸入密碼");
+// }
+
+// alert("登入成功")
+
+var link = document.getElementById("submit");
+
+
+console.log(link);
