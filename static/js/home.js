1ef3c3dbf1679babd40f0ca8dda2ef716e0327f5
TC <dg910719@gmail.com>
2022-04-22 15:21:29 +0800

    a lot of things

--- static/js/home.js
+++ static/js/home.js
@@ -1,12 +1,31 @@
-var grid = document.getElementById("grid");
-grid.src = ""
+
+var username;
+var team = {name:""};
 $(".content").css("dispaly", "none");
 $(".content").css("border", "0px");
+// var falg = new Boolean(false);
+var a = [];
+var b = [];
 
-var a = ['apple', 'banana'];
-// alert(a[1]);
+function reset(team) {
+    username = document.getElementById("Username").value;
+    a = [];
+    a.push(username);
+    alert(a[0]);
+    b = a;
+    alert("b[0] = " + b[0]);
+    team.name = username;
+}
+// alert("username = " + username);
+// alert(a[0]);
+// console.log(a);
+// alert("b[0] = " + a[0]);
 
-function home(){
+alert(team.name);
+
+document.getElementById("tn_display").innerHTML = "隊名:" + a[0];
+
+function home() {
     $("#f01").css("border", "0px");
     $("#f02").css("border", "0px");
     $("#f03").css("border", "0px");
@@ -17,7 +36,7 @@ function home(){
     grid.src = "";
 }
 
-function npc(){
+function npc() {
     $("#f06").css("border", "0px");
     $("#f02").css("border", "0px");
     $("#f03").css("border", "0px");
@@ -28,7 +47,7 @@ function npc(){
     grid.src = "./npc.html";
 }
 
-function get(){
+function input() {
     $("#f01").css("border", "0px");
     $("#f06").css("border", "0px");
     $("#f03").css("border", "0px");
@@ -36,10 +55,10 @@ function get(){
     $("#f05").css("border", "0px");
     $("#f02").css("border", "2px solid white");
     $(".content").css("border", "5px solid rgb(163, 60, 0)");
-    grid.src = "";
+    grid.src = "./input.html";
 }
 
-function bag(){
+function bag() {
     $("#f01").css("border", "0px");
     $("#f02").css("border", "0px");
     $("#f06").css("border", "0px");
@@ -50,7 +69,7 @@ function bag(){
     grid.src = "./npc.html";
 }
 
-function map(){
+function map() {
     $("#f01").css("border", "0px");
     $("#f02").css("border", "0px");
     $("#f03").css("border", "0px");
@@ -61,7 +80,7 @@ function map(){
     grid.src = "./npc.html";
 }
 
-function mission(){
+function mission() {
     $("#f01").css("border", "0px");
     $("#f02").css("border", "0px");
     $("#f03").css("border", "0px");
@@ -70,4 +89,18 @@ function mission(){
     $("#f05").css("border", "2px solid white");
     $(".content").css("border", "5px solid rgb(163, 60, 0)");
     grid.src = "./npc.html";
+}
+
+function A() {
+    alert("清除守衛");
+    var obj = document.getElementById("1");
+    var pobj = obj.parentNode;
+    // pobj.innerHTML = pobj.innerHTML + `
+    // <tr>
+    //     <th>NPC</th>
+    //     <th>介紹</th>
+    // </tr>
+    // `;
+    pobj.removeChild(obj);
+    // console.log(obj.innerHTML);
 }
